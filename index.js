e00c5251618b00bf5f8ae19f3572cb3184a3d8ea
Om Vaja <vajaom09@gmail.com>
2024-08-28 16:22:07 +0530

    http server created

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Initial setup
+const http = require('http');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const server = http.createServer((req, res) => {
+    res.write('Route Accessed');
+    res.end();
+});
+
+const PORT = process.env.PORT || 3000;;
+
+server.listen(PORT, () => {
+    console.log(`server running on ${PORT}`);
+})
